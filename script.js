997e301308849b760da09205e738a1ac1cfe2c24
Bruno e Sousa Alencar <brunoesousaalencar@gmail.com>
2022-09-11 18:35:45 -0300

    script, css

---
+++ script.js
@@ -0,0 +1,73 @@
+function botao1(){
+    var texto1 =document.querySelector(".texto1");
+
+    if(texto1.style.display === "none"){
+
+        texto1.style.display = "block"
+
+    } else{
+
+        texto1.style.display = "none"
+
+    }
+
+}
+
+function botao2(){
+    var texto2 =document.querySelector(".texto2");
+
+    if(texto2.style.display === "none"){
+
+        texto2.style.display = "block"
+
+    } else{
+
+        texto2.style.display = "none"
+
+    }
+
+}
+
+function botao3(){
+    var texto3 =document.querySelector(".texto3");
+
+    if(texto3.style.display === "none"){
+
+        texto3.style.display = "block"
+
+    } else{
+
+        texto3.style.display = "none"
+
+    }
+
+}
+
+function botao4(){
+    var texto4 =document.querySelector(".texto4");
+
+    if(texto4.style.display === "none"){
+
+        texto4.style.display = "block"
+
+    } else{
+
+        texto4.style.display = "none"
+
+    }
+
+}
+
+function botao5(){
+    var texto5 =document.querySelector(".texto5");
+    if(texto5.style.display === "none"){
+
+        texto5.style.display = "block"
+
+    } else{
+
+        texto5.style.display = "none"
+
+    }
+
+}
